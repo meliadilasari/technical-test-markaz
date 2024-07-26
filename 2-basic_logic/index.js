29219b648f2bf2773c5970e125ee615bebf198de
melia dilasari <meliadila1396@gmail.com>
2024-07-26 23:34:23 +0700

    Finish soal 3 slicing

--- 2-basic_logic/index.js
+++ 2-basic_logic/index.js
@@ -1,12 +1,13 @@
-function hashTriangle(x) {
+function createTriangle(x) {
   let result = "";
   for (let i = 0; i < x; i++) {
     for (let j = 0; j < x; j++) {
-      j + i >= 2 ? (result += "#") : (result += " ");
+      j + i >= x - 1 ? (result += "#") : (result += " ");
     }
     if (i !== x - 1) result += "\n";
   }
   return result;
 }
 
-console.log(hashTriangle(3));
+console.log(createTriangle(3));
+console.log(createTriangle(5));
